313793c80a9070954db4cdabba2328d403776bb0
Jason <123558105+JChow999@users.noreply.github.com>
2023-10-27 17:11:55 -0400

    Add button to reset grid. User can set the gridsize Change grid size button deletes grid and creates new one

--- javascript.js
+++ javascript.js
@@ -1,17 +1,45 @@
-body = document.querySelector('body')
-sketchGrid = document.querySelector('#sketchGrid')
+const DEFAULT_SIZE = 16;
+
+const body = document.querySelector('body')
+const sketchGrid = document.querySelector('#sketchGrid')
+const gridChange = document.querySelector('#gridChangeBtn')
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 
-let gridSize = 16
 
-for (let row = 0; row < gridSize; row++) {
-    const row = document.createElement('div');
-    row.setAttribute('class', 'gridColumn');
-    for (let column = 0; column < gridSize; column++) {
-        const newDiv = document.createElement('div');
-        newDiv.setAttribute('class', 'squareCont');
-        row.appendChild(newDiv);
-    }    
-    sketchGrid.appendChild(row)
+
+function deleteGrid() {
+    sketchGrid.innerHTML = ""
 }
 
+function createGrid(gridSize) {
+    for (let row = 0; row < gridSize; row++) {
+        const row = document.createElement('div');
+        row.setAttribute('class', 'gridColumn');
+        for (let column = 0; column < gridSize; column++) {
+            const newDiv = document.createElement('div');
+            newDiv.setAttribute('class', 'squareCont');
+            changeColour(newDiv)
+            row.appendChild(newDiv);
+        }    
+        sketchGrid.appendChild(row);
+    }
+}
+
+function changeColour(div) {
+    div.addEventListener('mousedown', () => {
+        div.style.backgroundColor = 'black';
+    });
+}
+
+
+createGrid(DEFAULT_SIZE);
+
+gridChange.addEventListener('click', () => {
+    let userInput = prompt("Please enter an integer of 2-100 to generate your grid.", "16");
+    deleteGrid()
+    createGrid(userInput)
+});
